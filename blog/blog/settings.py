cff39e939760f4588d87d001c7e946cf6da0acca
Gregory Pacheco <gpzim98@gmail.com>
2019-10-12 20:37:05 +0100

    Configurando static_root

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -81,7 +81,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
